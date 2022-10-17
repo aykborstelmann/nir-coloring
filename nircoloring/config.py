59b6bc9e53ea85a99c867193a9286dc489843ea6
Ayk Borstelmann <ayk.borstelmann@cronn.de>
2022-10-17 14:56:35 +0200

    Divide into train and test data

--- nircoloring/config.py
+++ nircoloring/config.py
@@ -6,15 +6,23 @@ load_dotenv()
 
 ROOT_DIR = os.path.join(os.path.dirname(abspath(__file__)), os.pardir)
 
-DATASET_OUT = os.getenv("DATASET_OUT") if os.getenv("DATASET_OUT") is not None else join(ROOT_DIR, "out/dataset/")
-DATASET_IMAGES = join(DATASET_OUT, "images")
 
-DATASET_DIRECTORY = join(ROOT_DIR, "data/dataset/")
-DATASET_SUBSET = join(DATASET_DIRECTORY, "dataset_subset.txt")
-DATASET_METADATA_FILE = join(DATASET_DIRECTORY, 'caltech_images.json')
+DATASET_TEMP = os.getenv("DATESET_TEMP") if os.getenv("DATESET_TEMP") is not None else join(ROOT_DIR, "tmp/dataset")
+DATASET_TEMP_IMAGES = join(DATASET_TEMP, "images")
+
+DATASET_OUT = os.getenv("DATASET_OUT") if os.getenv("DATASET_OUT") is not None else join(ROOT_DIR, "../pytorch-CycleGAN-and-pix2pix", "datasets/caltech")
+DATASET_TRAIN_A = join(DATASET_OUT, "trainA")
+DATASET_TRAIN_B = join(DATASET_OUT, "trainB")
+DATASET_TEST_A = join(DATASET_OUT, "testA")
+DATASET_TEST_B = join(DATASET_OUT, "testB")
+
+DATA_DIRECTORY = join(ROOT_DIR, "data/dataset")
+DATASET_SUBSET = join(DATA_DIRECTORY, "dataset_subset.txt")
+DATASET_TRAIN_TEST_SPLIT = join(DATA_DIRECTORY, "dataset_train_test.json")
+DATASET_METADATA_FILE = join(DATA_DIRECTORY, 'caltech_images.json')
 
 CALTECH_DOWNLOAD_IMAGE_URL_TEMPLATE = "https://lilablobssc.blob.core.windows.net/caltech-unzipped/cct_images/{filename}"
 
 
 def get_dataset_image_file(filename):
-    return abspath(join(DATASET_IMAGES, filename))
+    return abspath(join(DATASET_TEMP_IMAGES, filename))
--- nircoloring/dataset/caltech.py
+++ nircoloring/dataset/caltech.py
@@ -1,17 +1,31 @@
 import asyncio
+import dataclasses
 import imghdr
 import io
 import json
+import os
 import random
+from dataclasses import dataclass
+from typing import Dict
 
 import aiofiles
 import tqdm.asyncio
-from PIL import Image
+from PIL import Image, ImageChops
 from azure.storage.blob.aio import BlobClient, StorageStreamDownloader
 
 from nircoloring.config import *
+from nircoloring.config import DATASET_TRAIN_TEST_SPLIT
 
 IMAGE_DOWNLOAD_SIZE = 1024
+TRAIN_DATASET_PROPORTION = 0.8
+
+
+@dataclass
+class DatasetSubset:
+    trainA: list[str]
+    trainB: list[str]
+    testA: list[str]
+    testB: list[str]
 
 
 def load_metadata():
@@ -25,15 +39,56 @@ def create_random_database_subset(dataset, size=1000):
     return random.sample(images, size)
 
 
+def divide_into_nir_and_rgb(filenames):
+    nir_images = []
+    rgb_images = []
+    for filename in filenames:
+        if is_nir_image(filename):
+            nir_images.append(filename)
+        else:
+            rgb_images.append(filename)
+
+    return nir_images, rgb_images
+
+
 def create_random_image_filename_subset_and_save():
     metadata = load_metadata()
     subset = create_random_database_subset(metadata)
-    filenames = list(map(lambda data: data["file_name"], subset))
+    filenames = map(lambda data: data["file_name"].strip(), subset)
 
     os.makedirs(DATASET_OUT, exist_ok=True)
     with open(DATASET_SUBSET, "w+") as file:
         for filename in filenames:
             file.write(filename + "\n")
+
+
+async def fetch_or_move(sema, directory, filename):
+    target_directory = join(DATASET_OUT, directory)
+    filepath = join(target_directory, filename)
+    if os.path.exists(filepath):
+        return
+
+    async with sema:
+        tmp_filepath = join(DATASET_TEMP_IMAGES, filename)
+        if os.path.exists(tmp_filepath):
+            os.rename(tmp_filepath, filepath)
+            return
+
+        await fetch_file_from_blob(asyncio.BoundedSemaphore(1), filename, target_directory)
+
+
+def create_train_and_test_devision():
+    filenames = load_filenames()
+    nir_images, rgb_images = divide_into_nir_and_rgb(filenames)
+    dataset_subset = DatasetSubset(
+        trainA=nir_images[:int(len(nir_images) * TRAIN_DATASET_PROPORTION)],
+        trainB=rgb_images[:int(len(rgb_images) * TRAIN_DATASET_PROPORTION)],
+        testA=nir_images[int(len(nir_images) * TRAIN_DATASET_PROPORTION):],
+        testB=rgb_images[int(len(rgb_images) * TRAIN_DATASET_PROPORTION):]
+    )
+    os.makedirs(DATA_DIRECTORY, exist_ok=True)
+    with open(DATASET_TRAIN_TEST_SPLIT, "w+") as file:
+        json.dump(dataclasses.asdict(dataset_subset), file)
 
 
 def load_filenames():
@@ -42,8 +97,14 @@ def load_filenames():
     return [image.strip() for image in images]
 
 
-async def fetch_file(sema, filename):
-    filepath = os.path.join(DATASET_IMAGES, filename)
+def load_dataset_subset():
+    with open(DATASET_TRAIN_TEST_SPLIT, "r") as file:
+        json_file_as_dict: Dict = json.load(file)
+        return DatasetSubset(**json_file_as_dict)
+
+
+async def fetch_file_from_blob(sema, filename, directory):
+    filepath = os.path.join(directory, filename)
     if os.path.exists(filepath) and imghdr.what(filepath) == 'jpeg':
         return
 
@@ -60,7 +121,7 @@ async def fetch_file(sema, filename):
         async with aiofiles.open(filepath, "wb") as outfile:
             await outfile.write(buffer.getbuffer())
 
-    await fetch_file(sema, filename)
+    await fetch_file_from_blob(sema, filename, directory)
 
 
 def crop_square_from_center(image):
@@ -77,11 +138,11 @@ async def wrap_in_progress_bar(tasks):
 
 
 def download_files():
-    os.makedirs(DATASET_IMAGES, exist_ok=True)
+    os.makedirs(DATASET_TEMP_IMAGES, exist_ok=True)
     images = load_filenames()
 
     sema = asyncio.BoundedSemaphore(10)
-    tasks = [fetch_file(sema, url) for url in images]
+    tasks = [fetch_file_from_blob(sema, filename, DATASET_TEMP_IMAGES) for filename in images]
     asyncio.run(wrap_in_progress_bar(tasks))
 
 
@@ -91,5 +152,32 @@ def create_dataset_subset_and_download():
     download_files()
 
 
+def is_nir_image(filename):
+    img = Image.open(get_dataset_image_file(filename))
+    r, g, b = img.split()
+    return ImageChops.difference(r, g).getbbox() is None and ImageChops.difference(g, b).getbbox() is None
+
+
+def get_dimensions(filename):
+    img = Image.open(get_dataset_image_file(filename))
+    return f"{img.size[0]}x{img.size[1]}"
+
+
 if __name__ == '__main__':
-    create_dataset_subset_and_download()
+    if not os.path.exists(DATASET_SUBSET):
+        create_dataset_subset_and_download()
+        create_train_and_test_devision()
+
+    os.makedirs(DATASET_TEST_A, exist_ok=True)
+    os.makedirs(DATASET_TEST_B, exist_ok=True)
+    os.makedirs(DATASET_TRAIN_A, exist_ok=True)
+    os.makedirs(DATASET_TRAIN_B, exist_ok=True)
+
+    dataset_subset = load_dataset_subset()
+    sema = asyncio.BoundedSemaphore(10)
+    tasks = [
+        fetch_or_move(sema, directory, filename)
+        for directory, filenames in dataclasses.asdict(dataset_subset).items()
+        for filename in filenames
+    ]
+    asyncio.run(wrap_in_progress_bar(tasks))
