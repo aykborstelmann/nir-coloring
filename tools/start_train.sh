88806926cf0d829f6d0c9f9a6f6cd6172ec6204e
Ayk Borstelmann <ayk.borstelmann@cronn.de>
2022-11-14 10:33:32 +0100

    Use wandb

--- tools/start_train.sh
+++ tools/start_train.sh
@@ -4,7 +4,7 @@ source "${BASH_SOURCE%/*}"/common.sh
 source "${BASH_SOURCE%/*}"/install_dependencies.sh
 
 NETWORK_NAME=$1
-CONSTANT_NETWORK_ARGUMENTS="--save_epoch_freq=40 --netG unet_256 --norm instance --verbose"
+CONSTANT_NETWORK_ARGUMENTS="--save_epoch_freq=40 --netG unet_256 --norm instance --verbose --use_wandb --wandb_project_name cycle-gan"
 
 if [ ! $NETWORK_NAME ]; then
   echo "Usage: start_train <network-name>"
